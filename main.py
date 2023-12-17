261ff4fa69f38f955719649dcfbe274d1aa0a3af
lpopivnyak <136169348+lpopivnyak@users.noreply.github.com>
2023-12-17 18:26:55 +0200

    fwe

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import pygame
+
+
+pygame.init()
+
+
+WHITE = (255, 255, 255)
+
+
+WIDTH, HEIGHT = 800, 600
+
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Простий Шутер")
+
+game = True
+while game:
+
+    window.fill((123,123,123))
+    
+
+
+
+
 
